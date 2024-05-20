22e35c4da994baa78f05df1856bbb901e75de368
MCL <michael@moshavcapital.com>
2024-05-20 16:51:39 +0100

    Update index.js - added Amplify Libraries

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { CookiesProvider } from 'react-cookie';
 
+import "@aws-amplify/ui-react/styles.css"; // Ensure React UI libraries are styled correctly
+import { Amplify } from 'aws-amplify'
+import awsconfig from './aws-exports'
+Amplify.configure(awsconfig) // Configures the Amplify libraries with the cloud backend set up via the Amplify CLI
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <CookiesProvider defaultSetOptions={{ path: '/' }}>
     <App />
-    </CookiesProvider>
   </React.StrictMode>
 );
 
